956cdaf9657468a90550fd3f415692edf5dc1c11
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-09 08:40:17 +0530

    Add files via upload

---
+++ wide casting.java
@@ -0,0 +1,8 @@
+public class wide casting{
+public static void main(String[]args){
+int myNum= 8;
+float myFloat= myNum;         //Automatic Casting: int to float
+system.out.println(myNum);   // output: 8
+system.out.println(MyFloat); // output: 8.0
+}
+}
