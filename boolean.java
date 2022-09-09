d95af656a9637d904e22eb62cd30ba47cd8fd3f2
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-09 08:11:12 +0530

    Add files via upload

---
+++ boolean.java
@@ -0,0 +1,8 @@
+public class void boolean{
+public static void main(string[] args){
+boolean isBiscuitCookie= true;
+boolean isBitterTasty= false;
+system.out.println(isBisucitCookie);
+system.out.println(isBitterTasty);
+}
+}
