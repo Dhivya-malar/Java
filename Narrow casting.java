7701df59d12009d01fd75355cb0ce2c75660b4ac
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-09 08:40:55 +0530

    Add files via upload

---
+++ Narrow casting.java
@@ -0,0 +1,8 @@
+public class Narrow casting{
+public static void main(String[] args){
+double myDouble= 9.87;
+byte myNum= 9;    //Manual casting: double to byte
+system.out.println(myDouble);  //output: 9.87
+system.out.println(myNum);    // output: 9
+}
+}
