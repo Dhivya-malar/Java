d86db95ac05ef45637b0575de0b6d8fda2d0a10c
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-10 08:23:01 +0530

    Add files via upload

---
+++ StringMethods.java
@@ -0,0 +1,7 @@
+public class StringMethods{
+public static void main(String[] args){
+String txt= "Hello world";
+system.out.println(txt.toUpperCase());
+system.out.println(txt.toLowerCase());
+}
+}
