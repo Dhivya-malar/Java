e4c41d0878a6c97e46fc71d8da78d9026965874d
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-19 07:56:55 +0530

    Delete String concatenation.java

--- String concatenation.java
+++
@@ -1,7 +0,0 @@
-public class String concatenation {
-public static void main(String[] args) {
-String firstname="John";
-String second name="Janu";
-System.out.println(firstname + " " + lastname);
-}
-}
