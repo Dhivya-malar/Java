11836aca69c37cdd68d9a665ddf118a59f131414
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-09-19 07:55:21 +0530

    Add files via upload

---
+++ String concatenation.java
@@ -0,0 +1,7 @@
+public class String concatenation {
+public static void main(String[] args) {
+String firstname="John";
+String second name="Janu";
+System.out.println(firstname + " " + lastname);
+}
+}
