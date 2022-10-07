b8668dd288460bfa73cb2d242a9afa7c26e425fd
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-10-07 12:17:18 +0530

    Add files via upload

---
+++ ForEach_loop.java
@@ -0,0 +1,16 @@
+package com.Dhivya;
+
+public class ForEach_loop {
+	public static void main (String[] args) {
+	String[] animal= {"Lion","Tiger","Fox","Elephant"};
+	for(String i: animal) {
+		System.out.println(i);
+		
+		}
+			
+		}
+			}
+			
+
+
+
