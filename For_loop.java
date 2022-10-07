f6214aede51b669bb4a66dca66a5b28ff7655713
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-10-07 11:59:09 +0530

    Add files via upload

---
+++ For_loop.java
@@ -0,0 +1,15 @@
+public class For_loop {
+public static void main (String[] args) {
+for(int i =1; i<=4; i++) {
+	for(int j=3; j>=i; j--) {
+		System.out.print(i);
+	}
+		System.out.println();
+	
+	}
+		
+	}
+		}
+		
+			
+
