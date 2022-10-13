6445dc0e5466aead8998db43bad75d811e307449
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-10-13 13:07:39 +0530

    Add files via upload

---
+++ Patterns_A.java
@@ -0,0 +1,17 @@
+Package com.Dhivya;
+public class Pattern_A{
+public static void main(String[]args) {
+		for(int row=1; row<=5; row++) {
+			for(int col=1; col<=5; col++) {
+				if(row==1 || row==(5/2)+1 ||col==1 || col==5 )
+			System.out.print("*");
+			else
+			System.out.print(" ");
+			}
+		System.out.println();
+		
+		}
+		
+		}
+}
+
