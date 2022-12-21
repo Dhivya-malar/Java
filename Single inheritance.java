a28f50c6fee7a458d7d171be5a465a7a4f095954
Dhivya <112560508+Dhivya-malar@users.noreply.github.com>
2022-12-21 20:23:29 +0530

    Add files via upload

---
+++ Single inheritance.java
@@ -0,0 +1,27 @@
+package madurai;
+
+public class Garden_Owner {
+		protected int price=10;
+		private int seedCount=50;
+		protected void pickfruits() {
+			System.out.println("pick all types of fruits in my garden");
+		}
+		protected void entry() {
+			System.out.println("My relative people can only enter into my garden");
+			}
+		private void OrganicFarming() {
+			System.out.println("Organic farming is done in may garden.");
+		}
+	}
+
+package salem;
+import madurai.Garden_Owner;
+public class SonGardenOwner extends Garden_Owner {
+public static void main(String[] args) {
+	SonGardenOwner property=new SonGardenOwner();
+	property.pickfruits();
+	property.entry();
+}
+	
+
+}
